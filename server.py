c79d12af5b9f7f553a0359ce78437bf08648d606
ice3erg <164074032+ice3erg@users.noreply.github.com>
2025-06-14 20:19:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="static", html=True), name="static")  # Обслуживание index.html и admin.html
+
+# Монтирование статических файлов с явным путем
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Инициализация базы данных
 try:
